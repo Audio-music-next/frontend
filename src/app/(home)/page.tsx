20139fd6630bb6178d5bafd546374859bc217ea9
gusdinizmaia <gustavodiniz37@hotmail.com>
2023-07-14 15:11:49 -0300

    feat: presentation base completed

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,13 @@
+import { Presentation } from "../Components/Presentation";
 import { Header } from "../Components/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+      <main className="flex min-h-screen flex-col items-center justify-between">
+        <Presentation />
+      </main>
     </>
   );
 }
