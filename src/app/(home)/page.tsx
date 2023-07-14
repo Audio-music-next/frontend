0a145aacb9fac07ebf1055b0f637e4404f335960
gusdinizmaia <gustavodiniz37@hotmail.com>
2023-07-14 15:41:36 -0300

    feat: postIts base completed

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,15 @@
 import { Header } from "../Components/Header";
+import { PostItCard } from "../Components/PostItCard";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+      <main className="flex min-h-screen flex-col items-center justify-between">
+        <section>
+          <PostItCard />
+        </section>
+      </main>
     </>
   );
 }
