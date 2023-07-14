dddaf0f7586a550c2abaa2fda3c86f493e783484
gusdinizmaia <gustavodiniz37@hotmail.com>
2023-07-14 14:34:59 -0300

    feat: header base completed

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,10 @@
+import { Header } from "../Components/Header";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home</h1>
-    </main>
+    <>
+      <Header />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    </>
   );
 }
