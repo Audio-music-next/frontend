ac97a164c3c889f1429227b504e3cc8642c6c6a9
gusdinizmaia <gustavodiniz37@hotmail.com>
2023-07-15 09:45:58 -0300

    feat: base footer completed

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,18 @@
 import { Presentation } from "../Components/Presentation";
 import { Header } from "../Components/Header";
 import { PostItCard } from "../Components/PostItCard";
+import { Footer } from "../Components/Footer";
 
 export default function Home() {
   return (
     <>
       <Header />
       <main className="flex min-h-screen flex-col items-center justify-between">
+        <Presentation />
         <section>
           <PostItCard />
         </section>
-        <Presentation />
+        <Footer />
       </main>
     </>
   );
--- src/app/Components/Header/index.tsx
+++ src/app/Components/Header/index.tsx
@@ -3,7 +3,7 @@ import { SwitchStyle } from "../SwitchStyle";
 
 export const Header = () => {
   return (
-    <header className="flex justify-between items-center bg-header object-cover w-full h-52">
+    <header className="flex justify-between items-center bg-header object-cover w-full h-52 px-[5%]">
       <Logo />
       <SwitchStyle />
     </header>
