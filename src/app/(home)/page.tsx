c5fe2701910f88b08216aa97150e0cb60fbaf1a0
gusdinizmaia <gustavodiniz37@hotmail.com>
2023-07-16 10:32:26 -0300

    feat: player and style base completed

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,20 @@
 import { Presentation } from "../Components/Presentation";
 import { Header } from "../Components/Header";
 import { PostItCard } from "../Components/PostItCard";
+import { MusicCard } from "../Components/MusicCard";
 
 export default function Home() {
   return (
     <>
       <Header />
       <main className="flex min-h-screen flex-col items-center justify-between">
+        <Presentation />
         <section>
           <PostItCard />
         </section>
-        <Presentation />
+        <section>
+          <MusicCard />
+        </section>
       </main>
     </>
   );
