ba50ebc858e9e38548a7e3045d8c9f75a50aea1d
gusdinizmaia <gustavodiniz37@hotmail.com>
2023-07-24 21:12:00 -0300

    hotfix: responsive switer

--- src/Components/SwiperPostIts/index.tsx
+++ src/Components/SwiperPostIts/index.tsx
@@ -14,8 +14,6 @@ export const SwiperPostIts = ({ slidesPerView }: iSwiperPostIts) => {
       className="sm:w-auto"
       spaceBetween={30}
       slidesPerView={slidesPerView}
-      onSlideChange={() => console.log("slide change")}
-      onSwiper={(swiper) => console.log(swiper)}
     >
       {slidesPerView !== 4 && (
         <SlidePrevButton className="w-8 h-8 cursor-pointer bg-gray-1 rounded-full text-white absolute top-[42%] left-0 z-50 sm:w-12:h-12:top-[40%]" />
@@ -24,7 +22,7 @@ export const SwiperPostIts = ({ slidesPerView }: iSwiperPostIts) => {
         <SwiperSlide key={post.title}>
           <li
             key={post.title}
-            className=" max-w-[200px] max-h-[220px] flex flex-col gap-2 rounded-radius-2 relative bg-random-1 p-4 pt-3 my-0 mx-auto sm:max-w-[240px] xl:mx-0:left-7"
+            className=" max-w-[200px] max-h-[220px] flex flex-col gap-2 rounded-radius-2 relative bg-random-1 p-4 pt-3 my-0 mx-auto min-[400px]:max-w-[240px] xl:mx-0:left-7"
           >
             <MusicalNoteIcon className="text-gray-5 absolute top-3 left-3 w-7 h-7" />
             <h5 className="ml-8 text-body-1 font-600 text-gray-1 sm:text-heading-5 ">
